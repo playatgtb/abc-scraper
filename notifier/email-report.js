17dd611dc2a8003b7082c110358ec9e61574ca43
jhanink <jhanink@gmail.com>
2023-11-20 09:19:55 -0800

    update notifiers

--- notifier/email-report.js
+++ notifier/email-report.js
@@ -140,8 +140,8 @@ const convertDate = (date, toLocalFormat=false) => {
 
 var mailOptions = {
   from: '"ABC Weekly Report" jhanink+abcscraper@gmail.com',
-  to:'zergworld+abcscraper@gmail.com',
-  //to:'play+abcscraper@gtbilliards.com',
+  //to:'zergworld+abcscraper@gmail.com',
+  to:'play+abcscraper@gtbilliards.com',
   subject: "GT WEEKLY -- ABC Scraper Weekly Report",
   text: "WORKING",
 }
