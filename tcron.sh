5cbfb06333d20f4766bd6896a98aed6bffb6bf35
Good Times <play@gtbilliards.com>
2023-12-18 09:56:37 -0800

    update cron scripts

--- tcron.sh
+++ tcron.sh
@@ -1,10 +1,5 @@
 
 export PATH=/opt/homebrew/bin:/opt/homebrew/sbin:/Users/goodtimes/Library/Caches/fnm_multishells/17084_1702916903408/bin:/Users/goodtimes/Library/Application Support/fnm:/opt/homebrew/bin:/opt/homebrew/sbin:/usr/local/bin:/System/Cryptexes/App/usr/bin:/usr/bin:/bin:/usr/sbin:/sbin:/var/run/com.apple.security.cryptexd/codex.system/bootstrap/usr/local/bin:/var/run/com.apple.security.cryptexd/codex.system/bootstrap/usr/bin:/var/run/com.apple.security.cryptexd/codex.system/bootstrap/usr/appleinternal/bin
 
-echo "$USER" > ~/Desktop/cron.log && echo "$PATH" >> ~/Desktop/cron.log
-echo "$(which npm)" >> ~/Desktop/cron.log
-
-git pull origin main
-npm i
-npm run weekly
-
+echo "$USER" > ~/Desktop/tcron.log && echo "$PATH" >> ~/Desktop/tcron.log
+echo "$(which npm)" >> ~/Desktop/tcron.log
