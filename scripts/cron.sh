925802d8030fa9687e9242798de4c25a9751f3ce
Joe Hanink <jhanink@gmail.com>
2024-01-03 18:26:53 -0800

    update cron to include A/B test

--- scripts/cron.sh
+++ scripts/cron.sh
@@ -1,10 +1,14 @@
 
 export PATH="/opt/homebrew/bin:/opt/homebrew/sbin:/Users/goodtimes/Library/Caches/fnm_multishells/17084_1702916903408/bin:/Users/goodtimes/Library/Application Support/fnm:/opt/homebrew/bin:/opt/homebrew/sbin:/usr/local/bin:/System/Cryptexes/App/usr/bin:/usr/bin:/bin:/usr/sbin:/sbin:/var/run/com.apple.security.cryptexd/codex.system/bootstrap/usr/local/bin:/var/run/com.apple.security.cryptexd/codex.system/bootstrap/usr/bin:/var/run/com.apple.security.cryptexd/codex.system/bootstrap/usr/appleinternal/bin:$PATH"
 
-echo "$USER" > ~/Desktop/cron.log && echo "$PATH" >> ~/Desktop/cron.log
-echo "$(which npm)" >> ~/Desktop/cron.log
+echo date > ~/Desktop/cron.log
+echo "$USER" >> ~/Desktop/cron.log
+
 
 git pull origin main
 npm i
 npm run weekly
+echo "-- ran weekly" >> ~/Desktop/cron.log
+npm run weekly-ab-test
+echo "-- ran weekly ab-test" >> ~/Desktop/cron.log
 
