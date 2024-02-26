e07fa8febc13fa5abd83f3217ba1728a27153453
Joe Hanink <jhanink@gmail.com>
2024-02-26 11:17:31 -0800

    weekly processing

--- scripts/cron.sh
+++ scripts/cron.sh
@@ -1,7 +1,7 @@
 
 export PATH="/opt/homebrew/bin:/opt/homebrew/sbin:/Users/goodtimes/Library/Caches/fnm_multishells/17084_1702916903408/bin:/Users/goodtimes/Library/Application Support/fnm:/opt/homebrew/bin:/opt/homebrew/sbin:/usr/local/bin:/System/Cryptexes/App/usr/bin:/usr/bin:/bin:/usr/sbin:/sbin:/var/run/com.apple.security.cryptexd/codex.system/bootstrap/usr/local/bin:/var/run/com.apple.security.cryptexd/codex.system/bootstrap/usr/bin:/var/run/com.apple.security.cryptexd/codex.system/bootstrap/usr/appleinternal/bin:$PATH"
 
-echo date > ~/Desktop/cron.log
+echo `date` >> ~/Desktop/cron.log
 echo "$USER" >> ~/Desktop/cron.log
 
 
@@ -10,7 +10,4 @@ npm i
 
 npm run weekly
 echo "-- ran weekly" >> ~/Desktop/cron.log
-
-#npm run weekly-a
-#echo "-- ran weekly-a" >> ~/Desktop/cron.log
 
