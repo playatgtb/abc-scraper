631c4d97b99363c6a2a988eb22520f32f9c7f26f
Joe Hanink <jhanink@gmail.com>
2024-04-04 14:00:33 -0700

    weekly processing

--- scripts/cron.sh
+++ scripts/cron.sh
@@ -1,8 +1,8 @@
 
 export PATH="/opt/homebrew/bin:/opt/homebrew/sbin:/Users/goodtimes/Library/Caches/fnm_multishells/17084_1702916903408/bin:/Users/goodtimes/Library/Application Support/fnm:/opt/homebrew/bin:/opt/homebrew/sbin:/usr/local/bin:/System/Cryptexes/App/usr/bin:/usr/bin:/bin:/usr/sbin:/sbin:/var/run/com.apple.security.cryptexd/codex.system/bootstrap/usr/local/bin:/var/run/com.apple.security.cryptexd/codex.system/bootstrap/usr/bin:/var/run/com.apple.security.cryptexd/codex.system/bootstrap/usr/appleinternal/bin:$PATH"
 
-echo `date` >> ~/Desktop/cron.log
-echo "$USER" >> ~/Desktop/cron.log
+echo `date` >> ~/cron.log
+echo "$USER" >> ~/cron.log
 
 
 git pull origin main
